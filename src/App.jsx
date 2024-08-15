ac3023b7358ddc619232669b33eaef60f4efd963
guillerminarinaldi <guillerinaldi@hotmail.com>
2024-08-15 10:11:18 -0500

    estilos css app

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={LogoApp} className="logo" alt="Logo App" />
-        </a>
-      </div>
-      <h1>Proyecto E-commerce Rinaldi</h1>
-      <div className="card">
-        <Button />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <NavBar />
-      <ItemListConteiner texto="Lo importante no es que vengas sino que vuelvas"/>
+      <ItemListConteiner texto = "Lo importante no es que vengas sino que vuelvas"/>
     </>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,18 @@
-import { CartWidget } from "../CartWidget/CartWidget"
+import React from 'react';
+import './NavBar.css';
+import { CartWidget } from "../CartWidget/CartWidget";
+
 
 export const NavBar = () => {
     return (
-        <nav>
-            <img src="" alt=""/>
-            <ul>
-                <li>Link 1</li>
-                <li>Link 2</li>
-                <li>Link 3</li>
+        <nav className="navbar">
+            <img src="" alt="Logo" className="navbar-logo"/>
+            <ul className="navbar-menu">
+                <li><a href="#link1">Link 1</a></li>
+                <li><a href="#link2">Link 2</a></li>
+                <li><a href="#link3">Link 3</a></li>
             </ul>
             <CartWidget />
         </nav>
-    )
+    );
 }

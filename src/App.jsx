1d61d0be0b15dd241e2880bb34ef89d9db27c855
guillerminarinaldi <guillerinaldi@hotmail.com>
2024-09-13 14:36:58 -0500

    rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { NavBar } from "./components/NavBar/NavBar";
+import { NavBar } from './components/NavBar/NavBar';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import CartWidget, { Cart } from './components/CartWidget/CartWidget';

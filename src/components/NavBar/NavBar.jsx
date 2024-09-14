8444a56af7be828b155107b98615e6259548678f
guillerminarinaldi <guillerinaldi@hotmail.com>
2024-09-13 19:21:16 -0500

    actualizacion

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import './NavBar.css';
-import { CartWidget } from '../CartWidget/CartWidget';
+import CartWidget from '../CartWidget/CartWidget';
 
 export const NavBar = () => {
   return (
